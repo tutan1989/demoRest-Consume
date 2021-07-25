7e5591ca22882ba69221579bef9f70fa4063bdd6
tutan <tutan.essential@gmail.com>
2021-07-25 16:18:42 +0000

    first commit

---
+++ demoRest-Consume/src/main/java/com/itc/client/DemoRestConsumeApplication.java
@@ -0,0 +1,13 @@
+package com.itc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestConsumeApplication.class, args);
+	}
+
+}
---
+++ demoRest-Consume/src/test/java/com/itc/client/DemoRestConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestConsumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
